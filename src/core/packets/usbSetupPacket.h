00e6559d228cb331ab0b32e6da40146c5a8b9c9b
Tim Brocklehurst <Tim@CompositeElectronics.com>
2025-02-02 15:48:09 +0000

    Improved setup packet decoding

--- src/core/packets/usbSetupPacket.h
+++ src/core/packets/usbSetupPacket.h
@@ -7,7 +7,17 @@
 
 class usbSetupPacket{
   public:
-    uint8_t bmRequestType;
+    enum rqDir       { toDevice=0x00,    toHost=0x01 };
+    enum rqType      { standard=0x00,  usbClass=0x01,   vendor=0x02, reserved=0x03 };
+    enum rqRecipient {   device=0x00, interface=0x01, endpoint=0x02,    other=0x03 };
+
+    union{
+      uint8_t bmRequestType;
+	  rqDir   reqDir  : 1;
+	  rqType  reqType : 2;
+	  rqRecipient rec : 5;
+	};
+	
     union{
       uint8_t  bRequest;
       usbStandardRequestCode request;
